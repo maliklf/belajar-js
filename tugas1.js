d9953b227962e7790b0b6049b415a454dbff5167
Malik LF <malik.learnfile@gmail.com>
2025-01-19 10:50:33 +0900

    Menambahkan tugas1.js

---
+++ tugas1.js
@@ -0,0 +1,7 @@
+var namadepan = "mohamad"
+let namabelakang = "malik"
+const umur = 29
+
+console.log(namadepan)
+console.log(namabelakang)
+console.log(umur)
---
+++ tugas2.js
@@ -0,0 +1,20 @@
+var minus = -3;
+var Asal = "Banten";
+
+var untung = true;
+var pendapatan = 30.000;
+if(pendapatan>60.000)
+untung = true;
+else
+untung = false;
+
+var data = "laki-laki"
+data = null
+
+var ketua = ["mamat","asep","nanang"]
+
+console.log(minus);
+console.log(Asal);
+console.log(untung);
+console.log(data);
+console.log(ketua[1]);
