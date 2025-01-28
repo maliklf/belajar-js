3015f89f0a35f2ad38c8e3e2029563c014c92dec
Malik LF <malik.learnfile@gmail.com>
2025-01-28 13:36:25 +0900

    update tugas3.js

--- tugas3.js
+++ tugas3.js
@@ -1,1 +1,11 @@
+function cari(){
+    var deteksi = "didapat";
+    return deteksi
+}
+var data = cari();
+console.log(data);
 
+function perkenalan(nama){
+    console.log(nama);
+}
+perkenalan("Malik")
