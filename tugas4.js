579c2ebee89919432a2b62a6edbd57db711b5a42
Malik LF <malik.learnfile@gmail.com>
2025-02-01 17:33:05 +0900

    add tugas4.js

---
+++ tugas4.js
@@ -0,0 +1,37 @@
+function tinggiBadan(){
+    var siswa1 = 173;
+    var siswa2 = 171;
+    var siswa3 = 169;
+
+    if (siswa1>siswa2){
+        if (siswa1>siswa3){
+            console.log("siswa1 memiliki tinggi badan paling tinggi")
+        }else{
+            console.log("siswa1 memiliki tinggi badan tertingi kedua")
+        }
+    }else{
+        if (siswa1<siswa3){
+            console.log("siswa1 memiliki tinggi badan terpendek")
+        }else{
+            console.log("siswa1 memiliki tinggi badan tertingi kedua")
+        }
+        
+    }
+}
+
+tinggiBadan()
+
+// tidak tahu cara mengurutkan hasil dari yg tertinggi ke yang terpendek
+
+
+
+// var siswa1 = 173;
+// var siswa2 = 171;
+// var siswa3 = 169;
+
+// if (siswa1>siswa2)
+//     console.log("siswa1 memiliki tinggi badan paling tingi")
+// else if (siswa1>siswa3)
+//     console.log("siswa1 memiliki tinggi badan tertingi kedua")
+// else 
+//     console.log("siswa1 memiliki tinggi badan terpendek")
