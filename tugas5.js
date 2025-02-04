7bbfaec2867edfe91e81bd2e1f24e46a1d984e65
Malik LF <malik.learnfile@gmail.com>
2025-02-05 05:46:17 +0900

    Add tugas5.js

---
+++ tugas5.js
@@ -0,0 +1,32 @@
+for(var i=1; i<=20; i++){
+    // if(i==1)
+    // continue;
+    // if(i==2)
+    // continue;
+    // if(i==4)
+    // continue;
+    // if(i==5)
+    // continue;
+    // if(i==7)
+    // continue;
+    // if(i==8)
+    // continue;
+    // if(i==10)
+    // continue;
+    // if(i==11)
+    // continue;
+    // if(i==13)
+    // continue;
+    // if(i==14)
+    // continue;
+    // if(i==16)
+    // continue;
+    // if(i==17)
+    // continue;
+    // if(i==19)
+    // continue;
+    // if(i==20)
+    // continue;
+    if(i%3===0)
+    console.log(i)
+}
