5072aad879c78f2f8ee295db28d60657b2ee7420
Malik LF <malik.learnfile@gmail.com>
2025-02-07 19:21:19 +0900

    add tugas7.js

---
+++ tugas7.js
@@ -0,0 +1,8 @@
+var tinggiBadan =[165,167,168,169,170,171,172,173,174,175];
+for(var x=0; x<tinggiBadan.length; x++){
+    console.log(tinggiBadan[x])
+}
+console.log("")
+for(var y of tinggiBadan){
+    console.log(y)
+}
