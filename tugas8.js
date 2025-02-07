67dba331a67c5bf303b07e0e4421724cd092a804
Malik LF <malik.learnfile@gmail.com>
2025-02-07 19:21:41 +0900

    add tugas8.js

---
+++ tugas8.js
@@ -0,0 +1,11 @@
+function smartPhone(){
+    var android = {
+        brand:"xiaomi",
+        warna:"hitam",
+        harga: 5000000
+    }
+    android.warna = "putih"
+    console.log(android)
+}
+
+smartPhone()
