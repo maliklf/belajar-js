ea07b0543b08e693732d7a6c168b0d2c86e64319
Malik LF <malik.learnfile@gmail.com>
2025-02-08 09:51:06 +0900

    Add tugas9.js

---
+++ tugas9.js
@@ -0,0 +1,13 @@
+function kuliner(){
+    var jepang = {
+        nama:"takoyaki",
+        rasa:"gurih",
+        harga: 500
+    }
+    for(var x in jepang){
+        console.log(jepang[x])
+    }
+    
+}
+
+kuliner()
